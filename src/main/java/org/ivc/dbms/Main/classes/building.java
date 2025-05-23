772f526dedb5413c734252edc437cf8f25e21e39
ryanvo504 <ryanvo@ucsb.edu>
2025-05-23 16:49:12 -0700

    push java classes

--- src/main/java/org/ivc/dbms/Main/classes/building.java
+++ src/main/java/org/ivc/dbms/Main/classes/building.java
@@ -1,5 +1,22 @@
 package org.ivc.dbms.Main.classes;
 
 public class building {
-    
+    private String buildingCode;
+
+    public building(){
+    }
+
+    public building(String buildingCode)
+    {
+        this.buildingCode = buildingCode;
+    }
+
+    public String getBuildingCode() {
+        return buildingCode;
+    }
+
+    public void setBuildingCode(String buildingCode) {
+        this.buildingCode = buildingCode;
+    }
+
 }
