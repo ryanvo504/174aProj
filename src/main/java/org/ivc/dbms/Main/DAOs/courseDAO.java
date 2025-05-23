6e63de5483e6469cc8b554255a20061a210d52fc
ryanvo504 <ryanvo@ucsb.edu>
2025-05-23 15:41:17 -0700

    push course and courseDAO

--- src/main/java/org/ivc/dbms/Main/DAOs/courseDAO.java
+++ src/main/java/org/ivc/dbms/Main/DAOs/courseDAO.java
@@ -1,5 +1,89 @@
 package org.ivc.dbms.Main.DAOs;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ivc.dbms.Main.classes.course;
+
 public class courseDAO {
     
+    private Connection connection;
+
+    public courseDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean addCourse(course course) throws SQLException {
+        String sql = "INSERT INTO Course (course_number, title) VALUES (?, ?)";
+        
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, course.getCourseNumber());
+            pstmt.setString(2, course.getTitle());
+            
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+    }
+
+    public course getCourseByCourseNumber(String courseNumber) throws SQLException {
+        String sql = "SELECT course_number, title FROM Course WHERE course_number = ?";
+        
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, courseNumber);
+            
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    course course = new course();
+                    course.setCourseNumber(rs.getString("course_number"));
+                    course.setTitle(rs.getString("title"));
+                    return course;
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<course> getAllCourses() throws SQLException {
+        List<course> courses = new ArrayList<>();
+        String sql = "SELECT course_number, title FROM Course";
+        
+        try (PreparedStatement pstmt = connection.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            
+            while (rs.next()) {
+                course course = new course();
+                course.setCourseNumber(rs.getString("course_number"));
+                course.setTitle(rs.getString("title"));
+                courses.add(course);
+            }
+        }
+        return courses;
+    }
+
+    public boolean updateCourse(course course) throws SQLException {
+        String sql = "UPDATE Course SET title = ? WHERE course_number = ?";
+        
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, course.getTitle());
+            pstmt.setString(2, course.getCourseNumber()); 
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+    }
+
+    public boolean deleteCourse(String courseNumber) throws SQLException {
+        String sql = "DELETE FROM Course WHERE course_number = ?";
+        
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, courseNumber);
+            
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+    }
+
 }
--- src/main/java/org/ivc/dbms/Main/classes/course.java
+++ src/main/java/org/ivc/dbms/Main/classes/course.java
@@ -1,5 +1,53 @@
 package org.ivc.dbms.Main.classes;
 
 public class course {
-    
+
+    private String courseNumber;
+    private String title;
+
+    public course() {
+    }
+
+    public course(String courseNumber, String title) {
+        this.courseNumber = courseNumber;
+        this.title = title;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "course{" +
+               "courseNumber='" + courseNumber + '\'' +
+               ", title='" + title + '\'' +
+               '}'; 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        course course = (course) o;
+        return courseNumber.equals(course.courseNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return courseNumber.hashCode();
+    }
+
 }
